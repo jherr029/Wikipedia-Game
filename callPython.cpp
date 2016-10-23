f1af09c2d0ba449503e5a4502013e2fbfc4a846e
jherr029 <jherr029@ucr.edu>
2016-10-23 01:07:39 -0700

    Loop set. No increment though

--- callPython.cpp
+++ callPython.cpp
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 #include<list>
-#include<stack>
+#include<queue>
 
 std::string fixSpaces( std::string );
 std::string addWikiLink( std::string );
 
 void callPythonScript( std::string );
-std::string getArticles( std::string );
+std::string getArticles( std::string, std::list< std::queue < std::string > > & );
 
 int main( )
 {
+    std::list< std::queue <std::string> > listQueuArticles;
 
     std::string startFlag        = "Kakababu";
     std::string endFlag          = "Pattern";
@@ -25,12 +27,27 @@ int main( )
 
     //std::cout << "Ending Article: ";
     //std::cin >> endFlag;
-    
+  
     callPythonScript( startFlag );
-    getArticles( endFlag );
-    
-    std::cout << "Done" << std::endl;
 
+    while ( completeFlag != "complete" )
+    {
+       
+        completeFlag = getArticles( endFlag, listQueuArticles ); 
+        std::cout << listQueuArticles.front( ).front( ) << std::endl;
+
+        callPythonScript( listQueuArticles.front( ).front( ) );
+        
+        if ( !listQueuArticles.front( ).empty( ) )
+            listQueuArticles.front( ).pop( );
+        else
+        {
+            completeFlag = "complete";
+            std::cout << "EMPTY EMPTY EMPTY" << std::endl;
+        }
+    }
+   
+    std::cout << "Done" << std::endl;
 
     return 0;
 }
@@ -51,12 +68,15 @@ void callPythonScript( std::string article )
 
 }
 
-std::string getArticles( std::string endArticle )
+std::string getArticles( std::string endArticle, std::list< std::queue< std::string> > &articles )
 {
     std::ifstream myFile;
     std::string line;
+    std::string title;
 
-    std::list<std::string> listOfTitles;
+    std::queue< std::string > titleQueue;
+
+    //std::cout << "READING FROM FILE " << std::endl;
 
     myFile.open( "myFile" );
 
@@ -64,9 +84,15 @@ std::string getArticles( std::string endArticle )
     {
         while ( getline( myFile, line ) )
         {
-            listOfTitles.push_back( line );
+            std::stringstream ss( line );
+            ss >> title;
+            //std::cout << "INSERTING" << std::endl;
+            
+            //std::cout << " " << title << std::endl;
+            titleQueue.push( title );
+            //std::cout << "BREAK?" << std::endl;
 
-            if ( endArticle == line )
+            if ( endArticle == title )
             {
                 std::cout << "FOUND A MATCH" << std::endl;
                 return "match";
@@ -75,6 +101,9 @@ std::string getArticles( std::string endArticle )
             //std::cout << line << std::endl;
         }
     }
+    
+    //std::cout << "PUSHING THAT SHIT" << std::endl;
+    articles.push_back( titleQueue );
 
     myFile.close( );
 
@@ -84,5 +113,7 @@ std::string getArticles( std::string endArticle )
 
 std::string addWikiLink( std::string article )
 {
-    return ( "https://en.wikipedia.org/wiki/" + article );
+    std::string test = "\"https://en.wikipedia.org/wiki/" + article + "\"";
+
+    return test;
 }
--- test.py
+++ test.py
@@ -6,11 +6,12 @@ import sys
 print sys.argv[1]
 
 connection = urllib.urlopen( sys.argv[1] )
+#connection = urllib.urlopen( "https://en.wikipedia.org/wiki/Muzaffar_Ahmed_(politician)")
 
 dom = lxml.html.fromstring( connection.read( ) )
 
 
-print "Opening the file..."
+#print "Opening the file..."
 target = open( "myFile", 'w')
 
 target.truncate( )
@@ -22,11 +23,5 @@ for link in dom.xpath('//a/@href'): # select the url in href for all a tags(link
         target.write( "\n")
         #print link
 
-print "Give me the input"
-
-#line1 = raw_input("line 1: ")
-
-#print "writing to the file"
-
-print "Closing file"
+#print "Closing file"
 target.close( )
